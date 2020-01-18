be6500517989da16bc27bb6d841a01f4aa6d82d4
QDG <nasha@QDGs-MacBook-Air.local>
2020-01-18 11:29:33 -0700

    added first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const funBusClick = document.querySelector('.intro img');
+funBusClick.addEventListener('mouseenter', () => {
+    funBusClick.style.transform = 'scale(.8)';
+})
+funBusClick.addEventListener('mouseleave', () => {
+    funBusClick.style.transform = 'scale(1.0)';
+})
